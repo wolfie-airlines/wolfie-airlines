3ef0ca3ab13f601eac1bb80b389cfc4db3612ef2
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-03-27 15:29:14 +0100

    Impementacja Vectora na funkcjach

--- lab1/Vector.h
+++ lab1/Vector.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <sstream>
+#include <cstring>
+#include <functional>
 
 namespace library {
     class Vector {
@@ -16,8 +18,19 @@ namespace library {
             }
         }
 
+        Vector(const size_t size, const std::function<double(const size_t)>&f) : size(size), data(new double[size]) {
+            for (size_t i = 0; i < size; i++) {
+                data[i] = f(i);
+            }
+        }
+
         Vector(const library::Vector& v): size(v.size), data(new double[size]) {
             memcpy(data, v.data, sizeof(*data) * size);
+        }
+
+        Vector(library::Vector&& v)  noexcept : size(v.size), data(v.data) {
+            v.size = 0;
+            v.data = nullptr;
         }
 
         ~Vector() noexcept {
