d7abe60a5304bbfb9fb1ae2c90c361cd32da379b
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-03-30 00:52:45 +0100

    Przywrócenie poprawnych konstruktorów `biletów`

---
+++ Projekt/airport/tickets/Ticket.cpp
@@ -0,0 +1,27 @@
+#include "Ticket.h"
+#include "bsoncxx/json.hpp"
+#include <utility>
+
+Ticket::Ticket(
+        std::string  ticketId,
+        User passenger,
+        std::string  flightId,
+        double price
+) : ticketId(std::move(ticketId)), passenger(std::move(passenger)), flightId(std::move(flightId)), price(price)
+{}
+
+std::string Ticket::getTicketId() const {
+    return ticketId;
+}
+
+std::string Ticket::getPassengerUsername() const {
+    return passenger.getUsername();
+}
+
+std::string Ticket::getFlightId() const {
+    return flightId;
+}
+
+double Ticket::getPrice() const {
+    return price;
+}
---
+++ Projekt/airport/tickets/Ticket.h
@@ -0,0 +1,40 @@
+#ifndef AIRPORT_TICKET_H
+#define AIRPORT_TICKET_H
+
+#include <string>
+#include <vector>
+#include "mongocxx/v_noabi/mongocxx/client.hpp"
+#include "mongocxx/v_noabi/mongocxx/database.hpp"
+#include "mongocxx/v_noabi/mongocxx/collection.hpp"
+#include "../user/User.h"
+
+class Ticket {
+private:
+    std::string ticketId;
+    User passenger;
+    std::string flightId;
+    double price{};
+
+    mongocxx::client _client;
+    mongocxx::database _db;
+    mongocxx::collection _collection;
+public:
+    Ticket(const std::string& uri_str, const std::string& db_name, const std::string& collection_name)
+            : _client{mongocxx::uri{uri_str}}, _db{_client[db_name]}, _collection{_db[collection_name]} {}
+
+    Ticket(
+            std::string ticketId,
+            User passenger,
+            std::string flightId,
+            double price
+    );
+
+    std::string getTicketId() const;
+    std::string getPassengerUsername() const;
+    std::string getFlightId() const;
+    double getPrice() const;
+
+};
+
+
+#endif //AIRPORT_TICKET_H
