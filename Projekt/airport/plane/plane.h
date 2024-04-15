5f993c4bedbc72311aa867913b07ac852f2b6055
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-04-15 18:33:44 +0200

    Dodanie pokazywania zaoszczędzonych pieniędzy

---
+++ Projekt/airport/plane/plane.h
@@ -0,0 +1,13 @@
+#ifndef AIRPORT_PLANE_H
+#define AIRPORT_PLANE_H
+
+#include <vector>
+#include "../flights/FlightConnection.h"
+#include "../user/User.h"
+
+void processSeatSelectionAndPurchase(std::vector<int> seatsTaken,
+                                     FlightConnection& flightConnection,
+                                     FlightConnection& foundConnection,
+                                     User& user);
+
+#endif //AIRPORT_PLANE_H
