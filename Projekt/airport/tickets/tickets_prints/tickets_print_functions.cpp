b2e80b651ede102b79ad80ed4158dcb7d76019f1
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-04-16 16:12:06 +0200

    Nowy info print dla ostrzeżenia

--- Projekt/airport/tickets/tickets_prints/tickets_print_functions.cpp
+++ Projekt/airport/tickets/tickets_prints/tickets_print_functions.cpp
@@ -70,3 +70,28 @@ std::string displayMessageAndCaptureInput(const std::string& titleMessage, const
 
     return answer;
 }
+
+std::string displayWarningAndCaptureInput(const std::string& titleMessage, const std::string& textMessage) {
+    auto createScreen = [&] {
+        auto summary = ftxui::vbox({
+                                           ftxui::hbox({ftxui::paragraphAlignCenter(titleMessage)}) | color(ftxui::Color::Red),
+                                           ftxui::separator(),
+                                           ftxui::hbox({
+                                                               ftxui::text(textMessage) | ftxui::bold | color(ftxui::Color::RedLight)}),
+                                           ftxui::separator(),
+                                           ftxui::hbox({ ftxui::text(L"back. \U0001F519  Wróć do głównego menu.") | ftxui::color(ftxui::Color::CadetBlue) | ftxui::bold }),
+                                   });
+        auto document = ftxui::vbox({window(ftxui::paragraphAlignCenter("WOLFI AIRPORT ️ ✈"), summary)});
+        document = document | size(ftxui::WIDTH, ftxui::LESS_THAN, 80);
+        return std::make_shared<ftxui::Element>(document);
+    };
+
+    auto userScreen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(*createScreen()));
+    ftxui::Render(userScreen, *createScreen());
+    std::cout << userScreen.ToString() << '\0' << std::endl;
+
+    std::string answer;
+    std::cin >> answer;
+
+    return answer;
+}
