e380d162dcad04fa20b45f60f93fc26a90ae9992
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-04-21 22:49:52 +0200

    Prawie prawidłowa obsługa odpraw online

--- Projekt/airport/webpage/src/pages/CheckinParams.js
+++ Projekt/airport/webpage/src/pages/CheckinParams.js
@@ -2,35 +2,55 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/navigation/Navbar";
 import { Footer } from "../components/sections/Footer";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import TypewriterEffectSmooth from "../components/effects/TypewriterEffect";
+import BoardCardCanvas from "../components/canvas/BoardCardCanvas";
+import { motion } from "framer-motion";
+import { generateGateNumber } from "../components/functions/BoardCardFunctions";
+import { BackgroundBeams } from "../components/effects/BackgroundBeams";
 
 export const CheckinParams = () => {
   //   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const [type, setType] = useState("");
-  const [code, setCode] = useState("");
+  const [error, setError] = useState(false);
+  const [departure, setDeparture] = useState("");
+  const [departureTime, setDepartureTime] = useState("");
+  const [destination, setDestination] = useState("");
+  const [destinationTime, setDestinationTime] = useState("");
+  const [gateNumber, setGateNumber] = useState(0);
   const [message, setMessage] = useState("");
+  const [code, setCode] = useState(0);
   const { username, email, flightId, seats } = useParams();
+  const seatsArray = seats.split(",").map(Number);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://wolfie-airlines.vercel.app/odprawa/${username}/${email}/${flightId}/${seats}`
-      )
-      .then((response) => {
-        console.log(response.data);
-        setType(response.data.type);
-        setCode(response.data.code);
-        setMessage(response.data.message);
-        setTimeout(() => {
-          setLoading(false);
-        }, 3500);
+    fetch(
+      `http://localhost:5000/odprawa/${username}/${email}/${flightId}/${seats}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.type === "error") {
+          setError(true);
+          setMessage(data.error);
+          setCode(data.code);
+          setTimeout(() => {
+            setLoading(false);
+          }, 3500);
+        } else {
+          setMessage(data.message);
+          setDeparture(data.departure);
+          setDepartureTime(data.departureTime);
+          setDestination(data.destination);
+          setDestinationTime(data.destinationTime);
+          setGateNumber(generateGateNumber());
+          setTimeout(() => {
+            setLoading(false);
+          }, 3500);
+        }
       })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [email, flightId, seats, username]);
+      .catch((error) => console.log(error));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
@@ -39,141 +59,88 @@ export const CheckinParams = () => {
         <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
           <section>
             <div className="2xl:mx-32">
-              <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-6 flex max-lg:flex-col">
-                <div className="lg:ml-0 lg:text-start mx-auto max-w-screen-md text-center mb-8 lg:mb-12 ">
+              <div className="py-8 px-4 mx-auto max-w-screen-xl lg:pt-2 lg:pb-0 lg:px-6 flex max-lg:flex-col">
+                <div className="lg:text-center mx-auto max-w-screen-md text-center mb-8 lg:mb-2">
                   <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white firefox:font-inter">
-                    Odprawy online
+                    Witamy na odprawie!
                   </h2>
                   <p className="text-gray-300 lg:mx-auto font-inter">
                     Chcesz odprawić się <strong>online</strong>? <br />Z{" "}
                     <strong>Wolfie Airlines</strong> to możliwe! <br />
                     <br />
                   </p>
-                  <h2 className="font-inter mt-2 mb-1 text-lg tracking-tight font-semibold text-white">
-                    Co potrzebujesz?
-                  </h2>
-                  <p className="text-gray-300 lg:mx-auto font-inter max-w-screen-md">
-                    Do odprawy online jest obowiązkowy wygenerowany{" "}
-                    <strong>kod QR</strong>, <br /> który wyświetla się po
-                    zakupie biletów, bądź w naszej aplikacji, <br /> kiedy
-                    użytkownik wybierze opcję <strong>odprawy online</strong>.
-                    <br />
-                    <span className="text-blue-500 font-semibold">
-                      Wystarczy go zeskanować a magia zadzieje się sama!
-                    </span>
-                  </p>
                 </div>
-
-                <form className="max-w-sm justify-end flex-col items-end max-lg:justify-end max-lg:items-end max-lg:flex-col max-lg:mx-auto max-lg:pr-0">
-                  <h2 className="text-end max-lg:text-center mb-4 text-3xl tracking-tight font-bold text-white firefox:font-inter">
-                    Sprawdź czy masz wszystko potrzebne do odprawy:
-                  </h2>
-                  <div class="flex items-center justify-end mb-2">
-                    <label
-                      for="disabled-checked-checkbox"
-                      class="me-2 text-lg font-medium text-gray-100 dark:text-gray-100"
-                    >
-                      Pobraną aplikację
-                    </label>
-                    <input
-                      disabled
-                      checked
-                      id="disabled-checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                  <div class="flex items-center justify-end mb-2">
-                    <label
-                      for="disabled-checked-checkbox"
-                      class="me-2 text-lg font-medium text-gray-100 dark:text-gray-100"
-                    >
-                      Kupiony bilet
-                    </label>
-                    <input
-                      disabled
-                      checked
-                      id="disabled-checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                  <div class="flex items-center justify-end mb-2">
-                    <label
-                      for="disabled-checked-checkbox"
-                      class="me-2 text-lg font-medium text-gray-100 dark:text-gray-100"
-                    >
-                      Przydzielone miejsce w samolocie
-                    </label>
-                    <input
-                      disabled
-                      checked
-                      id="disabled-checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                  <div class="flex items-center justify-end mb-2">
-                    <label
-                      for="disabled-checked-checkbox"
-                      class="me-2 text-lg font-medium text-gray-100 dark:text-gray-100 whitespace-nowrap"
-                    >
-                      Uregulowane płatności (bagaż/zwierzęta/bilet)
-                    </label>
-                    <input
-                      disabled
-                      checked
-                      id="disabled-checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                  <div class="flex items-center justify-end mb-2">
-                    <label
-                      for="disabled-checked-checkbox"
-                      class="me-2 text-lg font-medium text-gray-100 dark:text-gray-100"
-                    >
-                      Wygodny kocyk i przekąski
-                    </label>
-                    <input
-                      disabled
-                      checked
-                      id="disabled-checked-checkbox"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-
-                  <div className="flex items-end justify-end">
-                    <button
-                      type="button"
-                      // onClick={() =>
-                      //   navigate("app-release-link")
-                      // }
-                      className="max-sm:text-xs max-xl:text-sm font-inter py-2 px-6 mt-5 text-white font-semibold bg-blue-600 rounded-lg hover:bg-blue-700"
-                    >
-                      Pobierz naszą aplikację
-                    </button>
-                  </div>
-                </form>
               </div>
             </div>
             {loading ? (
-              <TypewriterEffectSmooth
-                words={[
-                  { text: "Trwa " },
-                  { text: "odprawa " },
-                  { text: "online..." },
-                ]}
-              />
-            ) : null}
+              <div className="flex items-center justify-center">
+                <TypewriterEffectSmooth
+                  words={[
+                    { text: "Trwa " },
+                    { text: "odprawa " },
+                    { text: "online..." },
+                  ]}
+                />
+              </div>
+            ) : error ? (
+              <div>
+                <div className="flex items-center justify-center">
+                  <TypewriterEffectSmooth
+                    words={[
+                      { text: "Błąd " },
+                      { text: "odprawy " },
+                      { text: "online..." },
+                    ]}
+                  />
+                </div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 5 }}
+                  className="flex flex-col justify-center items-center"
+                >
+                  <p className="text-white font-bold text-lg">{message}</p>
+                  <p className="text-red-500 font-bold text-md">
+                    Kod błędu: {code}
+                  </p>
+                </motion.div>
+              </div>
+            ) : (
+              <div>
+                <div className="flex justify-center items-center">
+                  <TypewriterEffectSmooth
+                    words={[{ text: "Odprawa " }, { text: "zakończona! " }]}
+                  />
+                </div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 5 }}
+                  className="grid grid-cols-1 mb-2 z-[40]"
+                >
+                  {seatsArray.map((seat) => (
+                    <BoardCardCanvas
+                      key={seat}
+                      width={5643}
+                      height={2499}
+                      imageUrl="https://i.imgur.com/o2paJAJ.png"
+                      user={username}
+                      email={email}
+                      flightId={flightId}
+                      departure={departure}
+                      departureTime={departureTime}
+                      destination={destination}
+                      destinationTime={destinationTime}
+                      seat={seat}
+                      gateNumber={gateNumber}
+                    />
+                  ))}
+                </motion.div>
+              </div>
+            )}
           </section>
         </div>
+        <BackgroundBeams className="z-[30]" />
       </div>
       <Footer />
     </div>
