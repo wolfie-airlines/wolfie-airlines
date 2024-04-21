077e58038bb1f88c3b4f2c58adc02fb066fee8c2
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-04-21 12:29:13 +0200

    Dodanie bg dla Home

--- Projekt/airport/webpage/src/pages/Home.js
+++ Projekt/airport/webpage/src/pages/Home.js
@@ -8,10 +8,9 @@ import Nav from "../components/navigation/Navbar";
 import { Footer } from "../components/components/Footer";
 const Home = () => {
   return (
-    <div>
+    <div className="dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
       <Nav />
-
-      <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+      <div className="dark:bg-dot-white/[0.2] bg-dot-black/[0.2]flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
         <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4 max-md:h-[30rem]">
           <motion.div
             initial={{
@@ -37,8 +36,8 @@ const Home = () => {
               niesamowitymi podróżami.
             </p>
           </motion.div>
-          <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-          <div className="absolute w-full -bottom-20 h-72 md:h-full z-10 max-md:top-24 max-md:w-full">
+          <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-transparent to-white z-40" />
+          <div className="absolute w-full mx-auto max-md:ml-2 -bottom-20 h-68 md:h-full z-10 max-md:top-24 max-md:w-full">
             <World data={sampleArcs} globeConfig={globeConfig} />
           </div>
         </div>
