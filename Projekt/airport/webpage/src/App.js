67c72167766d5679c42f016711a53ac7f5da7bd2
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-04-21 12:30:20 +0200

    Naprawienie scrolla

--- Projekt/airport/webpage/src/App.js
+++ Projekt/airport/webpage/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import AnimatedRoutes from "./components/routes/AnimatedRoutes";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.style.overflowY = "auto";
+  }, []);
+
   return <AnimatedRoutes />;
 }
 
