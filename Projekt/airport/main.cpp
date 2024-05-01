ed2ce9b476f92bbb4d729deaac979d4a65fad2df
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-01 19:01:24 +0200

    Aktualizacja importów w mainie

--- Projekt/airport/main.cpp
+++ Projekt/airport/main.cpp
@@ -4,7 +4,7 @@
 #include "env/EnvParser.h"
 #include "authentication/Authentication.h"
 #include "flights/FlightConnection.h"
-#include "functions/handling_functions.h"
+#include "functions/MainHandler.h"
 
 int main(int argc, char* argv[]) {
     mongocxx::instance inst;
