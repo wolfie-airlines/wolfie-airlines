74a49809a463ee83a9397233478fb7bf5a3deefe
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-01 18:59:07 +0200

    Dodanie nagłówków funkcji ekstraktujących

--- Projekt/airport/luggage/item/ItemHandler.h
+++ Projekt/airport/luggage/item/ItemHandler.h
@@ -5,6 +5,9 @@
 #include "../../user/User.h"
 #include <vector>
 
+double getDoubleValue(const bsoncxx::document::view& item, const std::string& key);
+std::vector<std::string> getArrayValue(const bsoncxx::document::view& item, const std::string& key);
+std::string getStringValue(const bsoncxx::document::view& item, const std::string& key);
 std::vector<Item> getItems(User& user);
 
 #endif // ITEM_HANDLER_H
