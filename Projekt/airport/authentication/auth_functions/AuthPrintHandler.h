dfe26ac08a2c54233c50935955f9bfaea892c4a7
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-01 18:56:10 +0200

    Przeniesienie funkcji logowania/rejestrowania

---
+++ Projekt/airport/authentication/auth_functions/AuthPrintHandler.h
@@ -0,0 +1,11 @@
+#ifndef AIRPORT_AUTHPRINTHANDLER_H
+#define AIRPORT_AUTHPRINTHANDLER_H
+
+#include <string>
+#include <tuple>
+
+std::tuple<std::string, std::string, std::string, bool> registerUser();
+std::tuple<std::string, std::string, bool> login();
+
+
+#endif //AIRPORT_AUTHPRINTHANDLER_H
