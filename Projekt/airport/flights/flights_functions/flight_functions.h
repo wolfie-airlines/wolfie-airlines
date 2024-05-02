059a36486bfdac61d05fae684efdc0080c6531cc
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-02 15:08:19 +0200

    Dodanie nagłówków funkcji dla funkcji lotów

--- Projekt/airport/flights/flights_functions/flight_functions.h
+++ Projekt/airport/flights/flights_functions/flight_functions.h
@@ -4,6 +4,16 @@
 #include "../../user/user.h"
 #include "../flight_connection.h"
 
+void HandleFlightSearchByCity(FlightConnection &flight_connection, User &user);
+
+void HandleFlightSearchById(FlightConnection &flight_connection, User &user);
+
+void HandleFlightSearchByPrice(FlightConnection &flight_connection, User &user);
+
+void HandleFlightSearchByDeparture(FlightConnection &flight_connection, User &user);
+
+void HandleFlightSearchByDestination(FlightConnection &flight_connection, User &user);
+
 void HandleFlightOptions(FlightConnection &flight_connection, User &user);
 
 #endif  // FLIGHT_FUNCTIONS_H
