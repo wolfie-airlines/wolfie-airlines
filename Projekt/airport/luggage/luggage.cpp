d443b95d817a5b9f859eb4066352bd522e26d5f7
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-02 13:48:29 +0200

    Poprawki w klasie luggage

--- Projekt/airport/luggage/luggage.cpp
+++ Projekt/airport/luggage/luggage.cpp
@@ -6,13 +6,13 @@
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 
-std::tuple<bool, std::string> Luggage::confirmItems(User &user) {
+std::tuple<bool, std::string> Luggage::ConfirmItems(User &user) {
   ftxui::Elements elements;
   auto screen = ftxui::ScreenInteractive::TerminalOutput();
-  for (const auto &item : items) {
+  for (const auto &item : items_) {
     elements.push_back(
         ftxui::hbox(
-            ftxui::text("• " + item.getItemName()) | ftxui::bold) |
+            ftxui::text("• " + item.GetItemName()) | ftxui::bold) |
             color(ftxui::Color::LightSteelBlue1));
   }
 
@@ -53,13 +53,13 @@ std::tuple<bool, std::string> Luggage::confirmItems(User &user) {
 
   screen.Loop(component);
 
-  for (const auto &item : items) {
-    if (item.isForbidden() && item.getProfession() != user.profession_ && !item.getProfession().empty()) {
-      std::string message = "Przedmiot " + item.getItemName() + " jest zabroniony/dostępny tylko dla osób o zawodzie "
-          + item.getProfession() + "!";
+  for (const auto &item : items_) {
+    if (item.IsForbidden() && item.GetProfession() != user.profession_ && !item.GetProfession().empty()) {
+      std::string message = "Przedmiot " + item.GetItemName() + " jest zabroniony/dostępny tylko dla osób o zawodzie "
+          + item.GetProfession() + "!";
       return {false, message};
-    } else if (item.isForbidden() && item.getProfession().empty()) {
-      std::string message = "Przedmiot " + item.getItemName() + " jest zabroniony!";
+    } else if (item.IsForbidden() && item.GetProfession().empty()) {
+      std::string message = "Przedmiot " + item.GetItemName() + " jest zabroniony!";
       return {false, message};
     }
   }
@@ -71,24 +71,24 @@ std::tuple<bool, std::string> Luggage::confirmItems(User &user) {
   }
 }
 
-double Luggage::processItemsAndGetWeight() {
+double Luggage::ProcessItemsAndGetWeight() {
   using namespace ftxui;
 
-  std::vector<std::string> itemQuantities(items.size());
+  std::vector<std::string> itemQuantities(items_.size());
   std::vector<Component> itemInputs;
   auto screen = ScreenInteractive::TerminalOutput();
 
-  for (size_t i = 0; i < items.size(); ++i) {
+  for (size_t i = 0; i < items_.size(); ++i) {
     size_t index = i;
 
-    Component input_item_quantity = Input(&itemQuantities[index], "Podaj ilość dla " + items[index].getItemName());
+    Component input_item_quantity = Input(&itemQuantities[index], "Podaj ilość dla " + items_[index].GetItemName());
     input_item_quantity |= CatchEvent([&, index](const Event &event) {
       if (event.is_character() && (!std::isdigit(event.character()[0]) || itemQuantities[index].size() > 3)) {
         return true;
       }
-      totalWeight = 0;
-      for (size_t j = 0; j < items.size(); ++j) {
-        totalWeight += itemQuantities[j].empty() ? 0 : std::stoi(itemQuantities[j]) * items[j].getWeight();
+      total_weight_ = 0;
+      for (size_t j = 0; j < items_.size(); ++j) {
+        total_weight_ += itemQuantities[j].empty() ? 0 : std::stoi(itemQuantities[j]) * items_[j].GetWeight();
       }
       return false;
     });
@@ -110,11 +110,11 @@ double Luggage::processItemsAndGetWeight() {
 
   auto renderer = Renderer(component, [&] {
     Elements elements;
-    for (size_t i = 0; i < items.size(); ++i) {
-      elements.push_back(hbox(text(items[i].getItemName() + " : "), itemInputs[i]->Render()));
+    for (size_t i = 0; i < items_.size(); ++i) {
+      elements.push_back(hbox(text(items_[i].GetItemName() + " : "), itemInputs[i]->Render()));
     }
     elements.push_back(finalButton->Render());
-    elements.push_back(text("Waga bagażu: " + std::to_string(totalWeight)));
+    elements.push_back(text("Waga bagażu: " + std::to_string(total_weight_)));
     return vbox({
                     hbox({paragraph("Podaj ilość przedmiotów, które chcesz zabrać ze sobą:") | bold})
                         | color(Color::MediumOrchid1),
@@ -130,20 +130,20 @@ double Luggage::processItemsAndGetWeight() {
   screen.Loop(renderer);
 
   double weight = 0;
-  for (size_t i = 0; i < items.size(); ++i) {
+  for (size_t i = 0; i < items_.size(); ++i) {
     if (itemQuantities[i].empty() || itemQuantities[i] == "0") {
-      errorFunction("Nie podano ilości przedmiotu " + items[i].getItemName() + "!", "Spróbuj ponownie.");
+      PrintErrorMessage("Nie podano ilości przedmiotu " + items_[i].GetItemName() + "!", "Spróbuj ponownie.");
       return -1;
     }
 
-    weight += std::stoi(itemQuantities[i]) * items[i].getWeight();
+    weight += std::stoi(itemQuantities[i]) * items_[i].GetWeight();
   }
 
   return weight;
 }
 
-double Luggage::calculateOverweightFee(double weight) const {
+double Luggage::CalculateOverweightFee(double weight) const {
   double extraWeight = weight - 20;
-  double extraFee = extraWeight * overweightFeePerKg * euroToPln;
+  double extraFee = extraWeight * overweight_fee_per_kg_ * euro_to_pln_;
   return extraFee;
 }
--- Projekt/airport/luggage/luggage.h
+++ Projekt/airport/luggage/luggage.h
@@ -7,25 +7,25 @@
 class User;
 
 class Luggage {
-  std::vector<Item> items;
-  double totalWeight = 0.0;
+  std::vector<Item> items_;
+  double total_weight_ = 0.0;
 
  public:
   Luggage(
       const std::vector<Item> &items,
-      double totalWeight
-  ) : items(items), totalWeight(totalWeight) {}
+      double total_weight
+  ) : items_(items), total_weight_(total_weight) {}
 
-  double processItemsAndGetWeight();
+  double ProcessItemsAndGetWeight();
 
-  std::tuple<bool, std::string> confirmItems(User &user);
+  std::tuple<bool, std::string> ConfirmItems(User &user);
 
-  const double overweightFeePerKg = 2.0;
-  const double euroToPln = 4.32;
-  const double maxAllowedWeight = 32.0;
-  double maxWeight = 20.0;
+  const double overweight_fee_per_kg_ = 2.0;
+  const double euro_to_pln_ = 4.32;
+  const double max_allowed_weight_ = 32.0;
+  double max_weight_ = 20.0;
 
-  double calculateOverweightFee(double weight) const;
+  double CalculateOverweightFee(double weight) const;
 };
 
 #endif //AIRPORT_LUGGAGE_H
