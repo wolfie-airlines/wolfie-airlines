d30aa08d50d03c3a383d5f40250fb5df61e82684
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-03 15:53:02 +0200

    Nowy walidator

--- Projekt/airport/admin/admin_functions/admin_functions.h
+++ Projekt/airport/admin/admin_functions/admin_functions.h
@@ -12,9 +12,16 @@ std::string CaptureInputWithValidation(
     const std::string &message,
     const std::function<bool(const std::string &)> &validator);
 
+std::string CaptureLineWithValidation(
+    const std::string &title,
+    const std::string &message,
+    const std::function<bool(const std::string &)> &validator);
+
 bool ValidateFlightId(const std::string &flight_id);
 bool ValidateCity(const std::string &city);
 bool ValidateDate(const std::string &date);
 bool ValidateTime(const std::string &time);
 bool ValidatePrice(const std::string &price);
+bool ValidateNonEmpty(const std::string &input);
+bool ValidateSolution(const std::string &solution);
 #endif //AIRPORT_ADMIN_ADMIN_FUNCTIONS_ADMIN_FUNCTIONS_H_
