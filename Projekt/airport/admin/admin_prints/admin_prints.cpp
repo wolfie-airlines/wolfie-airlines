cee76a63d288c5bcee67798821065f0beeec8e51
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-05-03 14:53:27 +0200

    Wyświetlanie informacji o dodawanym locie

--- Projekt/airport/admin/admin_prints/admin_prints.cpp
+++ Projekt/airport/admin/admin_prints/admin_prints.cpp
@@ -32,4 +32,106 @@ void DisplayAdminMenu() {
   std::cout << screen.ToString() << '\0' << std::endl;
 }
 
+void DisplayAddingFlightInfo() {
+  auto summary = [&] {
+    auto content = ftxui::vbox({
+                                   ftxui::hbox({ftxui::text(L"UWAGA ADMINISTRATORZE!") | ftxui::bold})
+                                       | color(ftxui::Color::YellowLight),
+                                   ftxui::hbox({ftxui::text(L"Dodawanie lotu to nie lada bajka.")
+                                                    | ftxui::bold}) | color(ftxui::Color::Orange3),
+                                   ftxui::hbox({ftxui::text(
+                                       L"Musisz podać wszystkie potrzebne informacje, aby lot mógł zostać dodany.")
+                                                    | ftxui::bold}) | color(ftxui::Color::Orange1),
+                                   ftxui::vbox({
+                                                   ftxui::hbox({ftxui::text(L"POTRZEBNE INFORMACJE:") | ftxui::bold})
+                                                       | color(ftxui::Color::BlueLight),
+                                                   ftxui::hbox({
+                                                                   ftxui::text(L"- ID LOTU ")
+                                                                       | ftxui::bold | color(ftxui::Color::YellowLight),
+                                                                   ftxui::paragraph(
+                                                                       "(Unikalne ID lotu): konwencja nazewnictwa to: DWIE PIERWSZE LITERY MIASTA WYLOTU - DWIE PIERWSZE LITERY MIASTA PRZYLOTU)")
+                                                                       | ftxui::bold | color(ftxui::Color::Yellow2),
+                                                                   ftxui::text(
+                                                                       " np. lot z Warszawy do Krakowa ma ID WA-KR")
+                                                                       | ftxui::bold | color(ftxui::Color::SkyBlue1),
+                                                               }),
+                                                   ftxui::hbox({ftxui::text(L"- Miasto odlotu")
+                                                                    | ftxui::bold}) | color(ftxui::Color::YellowLight),
+                                                   ftxui::hbox({ftxui::text(
+                                                       L"- Miasto przylotu") | ftxui::bold})
+                                                       | color(ftxui::Color::YellowLight),
+                                                   ftxui::hbox({
+                                                                   ftxui::text(
+                                                                       L"- Czas odlotu ")
+                                                                       | color(ftxui::Color::YellowLight),
+                                                                   ftxui::text(
+                                                                       L"(format: DD.MM.YYYY HH:MM)")
+                                                                       | color(ftxui::Color::Yellow2)
+                                                                       | ftxui::bold}),
+                                                   ftxui::hbox({
+                                                                   ftxui::text(
+                                                                       L"- Czas przylotu ")
+                                                                       | color(ftxui::Color::YellowLight),
+                                                                   ftxui::text(
+                                                                       L"(format: DD.MM.YYYY HH:MM)")
+                                                                       | color(ftxui::Color::Yellow2)
+                                                                       | ftxui::bold}),
+                                                   ftxui::hbox({
+                                                                   ftxui::text(L"- Cena lotu ")
+                                                                       | color(ftxui::Color::YellowLight),
+                                                                   ftxui::text(L"(w PLN)")
+                                                                       | color(ftxui::Color::Yellow2)
+                                                                       | ftxui::bold}),
+                                               }) | ftxui::border,
+                                   ftxui::separator(),
+                                   ftxui::hbox({ftxui::text(L"Jeśli chcesz przerwać dodawanie lotu wpisz:")
+                                                    | ftxui::bold}) | color(ftxui::Color::RedLight),
+                                   ftxui::hbox({ftxui::text(L"quit") | ftxui::bold}) | color(ftxui::Color::Red1),
+                                   ftxui::separator(),
+                                   ftxui::hbox({ftxui::text(L"Jeśli chcesz kontynuować dodawanie lotu wpisz:")
+                                                    | ftxui::bold}) | color(ftxui::Color::GreenLight),
+                                   ftxui::hbox({ftxui::text(L"dalej") | ftxui::bold}) | color(ftxui::Color::Green1)
+                               });
+    return window(ftxui::paragraphAlignCenter("WOLFI AIRPORT ️ ✈"), content);
+  };
+
+  auto document = ftxui::vbox({summary()});
+
+  document = document | size(ftxui::WIDTH, ftxui::LESS_THAN, 120);
+
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Fit(document), ftxui::Dimension::Fit(document));
+  ftxui::Render(screen, document);
+  std::cout << screen.ToString() << '\0' << std::endl;
+}
+
+std::string DisplayAdminMessageAndCaptureInput(const std::string &title_message, const std::string &text_message) {
+  auto create_screen = [&] {
+    auto summary = ftxui::vbox({
+                                   ftxui::hbox({ftxui::paragraphAlignCenter(title_message)})
+                                       | color(ftxui::Color::White),
+                                   ftxui::separator(),
+                                   ftxui::hbox({ftxui::text(text_message) | ftxui::bold
+                                                    | color(ftxui::Color::GrayLight)}),
+                                   ftxui::separator(),
+                                   ftxui::hbox({ftxui::text(L"back. \U0001F519  Wróć do panelu administratora.")
+                                                    | ftxui::color(ftxui::Color::CadetBlue) | ftxui::bold}),
+                               });
+    auto document = ftxui::vbox({window(ftxui::paragraphAlignCenter("WOLFI AIRPORT ️ ✈"), summary)});
+    document = document | size(ftxui::WIDTH, ftxui::LESS_THAN, 80);
+    return std::make_shared<ftxui::Element>(document);
+  };
+
+  auto user_screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(*create_screen()));
+  ftxui::Render(user_screen, *create_screen());
+  std::cout << user_screen.ToString() << '\0' << std::endl;
+
+  std::string answer;
+  std::cin >> answer;
+
+  return answer;
+}
+
+
+
+
 
--- Projekt/airport/admin/admin_prints/admin_prints.h
+++ Projekt/airport/admin/admin_prints/admin_prints.h
@@ -2,5 +2,7 @@
 #define AIRPORT_ADMIN_ADMIN_PRINTS_ADMIN_PRINTS_H_
 
 void DisplayAdminMenu();
+void DisplayAddingFlightInfo();
+std::string DisplayAdminMessageAndCaptureInput(const std::string &title_message, const std::string &text_message);
 
 #endif //AIRPORT_ADMIN_ADMIN_PRINTS_ADMIN_PRINTS_H_
