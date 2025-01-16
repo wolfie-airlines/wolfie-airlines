cb6d82032f723434c682b14b2ab1a85c9f21304e
Szymon Wilczek <szymonwilczek@outlook.com>
2025-01-16 18:59:43 +0100

    wiele zmian
    - dodanie iteratora
    - zmiana formy autentykacji bazy danych
    - uogólnienie klasy drzewa binarnego na poczet innych funkcji i klas
    - inicjalizacja drzewa binarnego obiektem uzytkownika

--- admin/admin_functions/managing_flights/flight_binary_tree.h
+++ admin/admin_functions/managing_flights/flight_binary_tree.h
@@ -9,58 +9,57 @@
 #include "custom_flight.h"
 #include "../../../user/user.h"
 
+template<typename T>
 class BinaryTree {
     struct Node {
-        std::shared_ptr<Flight> flight;
+        std::shared_ptr<T> data;
         std::unique_ptr<Node> left;
         std::unique_ptr<Node> right;
-        explicit Node(std::shared_ptr<Flight> f) : flight(std::move(f)), left(nullptr), right(nullptr) {}
+        explicit Node(std::shared_ptr<T> d) : data(std::move(d)), left(nullptr), right(nullptr) {}
     };
 
     std::unique_ptr<Node> root;
-    mongocxx::uri uri = mongocxx::uri{"mongodb+srv://wolfiksw:PrincePolo1@testcluster.taymr.mongodb.net/projekt"};
-    mongocxx::client client{uri};
 
-    mongocxx::database db = client["projekt"];
-    mongocxx::collection collection = db["flight_connections"];
 
-    void addFlightToTree(std::unique_ptr<Node>& node, std::shared_ptr<Flight> flight) {
+    User& admin;
+
+    void addDataToTree(std::unique_ptr<Node>& node, std::shared_ptr<T> data) {
         if (!node) {
-            node = std::make_unique<Node>(std::move(flight));
-        } else if (flight->flight_id < node->flight->flight_id) {
-            addFlightToTree(node->left, std::move(flight));
+            node = std::make_unique<Node>(std::move(data));
+        } else if (data->flight_id < node->data->flight_id) {
+            addDataToTree(node->left, std::move(data));
         } else {
-            addFlightToTree(node->right, std::move(flight));
+            addDataToTree(node->right, std::move(data));
         }
     }
 
-    [[nodiscard]] static std::shared_ptr<Flight> findFlightInTree(const std::unique_ptr<Node>& node, const std::string& id) {
+    [[nodiscard]] static std::shared_ptr<T> findDataInTree(const std::unique_ptr<Node>& node, const std::string& id) {
         if (!node) {
             return nullptr;
         }
-        if (node->flight->flight_id == id) {
-            return node->flight;
+        if (node->data->flight_id == id) {
+            return node->data;
         }
-        if (id < node->flight->flight_id) {
-            return findFlightInTree(node->left, id);
+        if (id < node->data->flight_id) {
+            return findDataInTree(node->left, id);
         }
-        return findFlightInTree(node->right, id);
+        return findDataInTree(node->right, id);
     }
 
     void inOrderTraversal(const std::unique_ptr<Node>& node, std::ofstream& file) const {
         if (!node) return;
         inOrderTraversal(node->left, file);
-        file << node->flight->flight_id << " " << node->flight->departure_city << " " << node->flight->destination_city << " "
-             << node->flight->departure_time << " " << node->flight->arrival_time << " " << node->flight->price << "\n";
+        file << node->data->flight_id << " " << node->data->departure_city << " " << node->data->destination_city << " "
+             << node->data->departure_time << " " << node->data->arrival_time << " " << node->data->price << "\n";
         inOrderTraversal(node->right, file);
     }
 
-    void removeFlightFromTree(std::unique_ptr<Node>& node, const std::string& id) {
+    void removeDataFromTree(std::unique_ptr<Node>& node, const std::string& id) {
         if (!node) return;
-        if (id < node->flight->flight_id) {
-            removeFlightFromTree(node->left, id);
-        } else if (id > node->flight->flight_id) {
-            removeFlightFromTree(node->right, id);
+        if (id < node->data->flight_id) {
+            removeDataFromTree(node->left, id);
+        } else if (id > node->data->flight_id) {
+            removeDataFromTree(node->right, id);
         } else {
             if (!node->left) {
                 node = std::move(node->right);
@@ -68,8 +67,8 @@ class BinaryTree {
                 node = std::move(node->left);
             } else {
                 const auto minNode = findMin(node->right);
-                node->flight = minNode->flight;
-                removeFlightFromTree(node->right, minNode->flight->flight_id);
+                node->data = minNode->data;
+                removeDataFromTree(node->right, minNode->data->flight_id);
             }
         }
     }
@@ -83,7 +82,7 @@ class BinaryTree {
     }
 
     void initializeTree() {
-        for (mongocxx::cursor cursor = collection.find({}); auto&& doc : cursor) {
+        for (mongocxx::cursor cursor = admin.GetSpecificCollection("flight_connections").find({}); auto&& doc : cursor) {
             auto flight_id = static_cast<std::string>(doc["identifier"].get_string().value);
             auto departure_city = static_cast<std::string>(doc["departureCity"].get_string().value);
             auto destination_city = static_cast<std::string>(doc["destinationCity"].get_string().value);
@@ -92,47 +91,103 @@ class BinaryTree {
             int available_seats = doc["availableSeats"].get_int32().value;
             int price = doc["price"].get_int32().value;
 
-            const auto flight = std::make_shared<Flight>(flight_id, departure_city, destination_city, departure_time, arrival_time, available_seats, price);
-            addFlightToTree(root, flight);
+            const auto data = std::make_shared<T>(flight_id, departure_city, destination_city, departure_time, arrival_time, available_seats, price);
+            addDataToTree(root, data);
         }
     }
 
 public:
-    BinaryTree() {
+    explicit BinaryTree(User& user) : root(nullptr), admin(user) {
         initializeTree();
     }
 
-    void addFlight(const std::shared_ptr<Flight>& flight) {
-        addFlightToTree(root, flight);
+    void addData(const std::shared_ptr<T>& data) {
+        addDataToTree(root, data);
 
-        bsoncxx::builder::basic::document flight_builder{};
-        flight_builder.append(
-        bsoncxx::builder::basic::kvp("identifier", flight->flight_id),
-        bsoncxx::builder::basic::kvp("departureCity", flight->departure_city),
-        bsoncxx::builder::basic::kvp("destinationCity", flight->destination_city),
-        bsoncxx::builder::basic::kvp("departureTime", flight->departure_time),
-        bsoncxx::builder::basic::kvp("arrivalTime", flight->arrival_time),
-        bsoncxx::builder::basic::kvp("price", flight->price),
-        bsoncxx::builder::basic::kvp("availableSeats", flight->available_seats),
+        bsoncxx::builder::basic::document data_builder{};
+        data_builder.append(
+        bsoncxx::builder::basic::kvp("identifier", data->flight_id),
+        bsoncxx::builder::basic::kvp("departureCity", data->departure_city),
+        bsoncxx::builder::basic::kvp("destinationCity", data->destination_city),
+        bsoncxx::builder::basic::kvp("departureTime", data->departure_time),
+        bsoncxx::builder::basic::kvp("arrivalTime", data->arrival_time),
+        bsoncxx::builder::basic::kvp("price", data->price),
+        bsoncxx::builder::basic::kvp("availableSeats", data->available_seats),
         bsoncxx::builder::basic::kvp("seatsTaken", bsoncxx::builder::basic::make_array().view())
         );
-        collection.insert_one(flight_builder.view());
+
+        admin.GetSpecificCollection("flight_connections").insert_one(data_builder.view());
     }
 
-    [[nodiscard]] std::shared_ptr<Flight> findFlightById(const std::string& id) const {
-        return findFlightInTree(root, id);
+    [[nodiscard]] std::shared_ptr<T> findDataById(const std::string& id) const {
+        return findDataInTree(root, id);
     }
 
-    void removeFlight(const std::string& id) {
-        removeFlightFromTree(root, id);
+    void removeData(const std::string& id) {
+        removeDataFromTree(root, id);
         bsoncxx::builder::basic::document filter_builder{};
         filter_builder.append(bsoncxx::builder::basic::kvp("identifier", id));
-        collection.delete_one(filter_builder.view());
+        admin.GetSpecificCollection("flight_connections").delete_one(filter_builder.view());
     }
 
     void serialize(const std::string& filename) const {
         std::ofstream file(filename);
         inOrderTraversal(root, file);
+    }
+
+    class Iterator {
+        struct StackNode {
+            Node* node;
+            StackNode* next;
+        };
+
+        StackNode* stackTop;
+
+        void push(Node* node) {
+            auto* newNode = new StackNode{node, stackTop};
+            stackTop = newNode;
+        }
+
+        Node* pop() {
+            if (!stackTop) return nullptr;
+            StackNode* topNode = stackTop;
+            stackTop = stackTop->next;
+            Node* node = topNode->node;
+            delete topNode;
+            return node;
+        }
+
+        void pushLeft(Node* node) {
+            while (node) {
+                push(node);
+                node = node->left.get();
+            }
+        }
+
+    public:
+        explicit Iterator(Node* root) : stackTop(nullptr) {
+            pushLeft(root);
+        }
+
+        ~Iterator() {
+            while (stackTop) {
+                pop();
+            }
+        }
+
+        [[nodiscard]] bool hasNext() const {
+            return stackTop != nullptr;
+        }
+
+        std::shared_ptr<T> next() {
+            Node* node = pop();
+            pushLeft(node->right.get());
+            return node->data;
+        }
+    };
+
+    Iterator begin() {
+        return Iterator(root.get());
     }
 };
 
